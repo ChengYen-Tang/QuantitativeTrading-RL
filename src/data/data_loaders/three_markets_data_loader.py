82f5b4db25e1c806c2f37d47a3682b1e360c0e48
KennethTang <kenneth@nssh.onmicrosoft.com>
2021-11-01 00:12:53 +0800

    Three markets data loader

--- src/data/data_loaders/three_markets_data_loader.py
+++ src/data/data_loaders/three_markets_data_loader.py
@@ -5,7 +5,10 @@ from src.data.data_loaders.kline_data_loader import *
 
 class three_markets_data_loader(kline_data_loader):
     def load_csv_data(self, coin1_path, coin2_path, coin1_2_coin2_path):
-        return self.__load_csv_data(coin1_path, coin2_path, coin1_2_coin2_path)
+        coin1_df, coin2_df, coin1_2_coin2_df = self.__load_csv_data(coin1_path, coin2_path, coin1_2_coin2_path)
+        df = coin1_df.merge(coin2_df, on='date', suffixes=('_coin1', '_coin2'))
+        df = df.merge(coin1_2_coin2_df, on='date', suffixes=('', '_coin1_2_coin2'), sort=True)
+        return df
 
     def __load_csv_data(self, coin1_path, coin2_path, coin1_2_coin2_path):
         process_pool = Pool()
@@ -18,7 +21,7 @@ class three_markets_data_loader(kline_data_loader):
 
 if __name__ == '__main__':
     data_loader = three_markets_data_loader()
-    coin1_df, coin2_df, coin1_2_coin2_df = data_loader.load_csv_data('./tests/test_files/BTCUSDT-Spot.csv', './tests/test_files/ETHUSDT-Spot.csv', './tests/test_files/ETHBTC-Spot.csv')
+    coin1_df, coin2_df, coin1_2_coin2_df = data_loader._three_markets_data_loader__load_csv_data('./tests/test_files/BTCUSDT-Spot.csv', './tests/test_files/ETHUSDT-Spot.csv', './tests/test_files/ETHBTC-Spot.csv')
     print(coin1_df)
     print(coin2_df)
     print(coin1_2_coin2_df)
--- tests/data/data_loaders/test_three_markets_data_loader.py
+++ tests/data/data_loaders/test_three_markets_data_loader.py
@@ -1,20 +1,38 @@
-import pytest
+import sys
+import pandas as pd
+sys.path.append('./')
 from src.data.data_loaders.three_markets_data_loader import *
 
-def setup_function():
-    data_loader = three_markets_data_loader()
-    pytest.coin1_df, pytest.coin2_df, pytest.coin1_2_coin2_df = data_loader.load_csv_data('./tests/test_files/BTCUSDT-Spot.csv', './tests/test_files/ETHUSDT-Spot.csv', './tests/test_files/ETHBTC-Spot.csv')
+class Test_csv_lorder:
+    def setup_method(self):
+        data_loader = three_markets_data_loader()
+        self.df = data_loader.load_csv_data('./tests/test_files/BTCUSDT-Spot.csv', './tests/test_files/ETHUSDT-Spot.csv', './tests/test_files/ETHBTC-Spot.csv')
 
-def test_deduplication():
-    print(pytest.coin1_df)
-    assert len(pytest.coin1_df) == 309559
-    assert len(pytest.coin2_df) == 309559
-    assert len(pytest.coin1_2_coin2_df) == 309559
+    def test_sort(self):
+        results = [self.df['date'][index - 1] < self.df['date'][index] for index in range(1, len(self.df))]
+        assert all(results)
 
-def test_sort():
-    coin1_results = [pytest.coin1_df['date'][index - 1] < pytest.coin1_df['date'][index] for index in range(1, len(pytest.coin1_df))]
-    coin2_results = [pytest.coin2_df['date'][index - 1] < pytest.coin2_df['date'][index] for index in range(1, len(pytest.coin2_df))]
-    coin1_2_coin2_results = [pytest.coin1_2_coin2_df['date'][index - 1] < pytest.coin1_2_coin2_df['date'][index] for index in range(1, len(pytest.coin1_2_coin2_df))]
-    assert all(coin1_results)
-    assert all(coin2_results)
-    assert all(coin1_2_coin2_results)
+    def test_length(self):
+        assert len(self.df) == 309557
+
+    def test_data_no_nan(self):
+        results = pd.notna(self.df)
+        assert all(results)
+
+class Test_private_csv_lorder:
+    def setup_method(self):
+        data_loader = three_markets_data_loader()
+        self.coin1_df, self.coin2_df, self.coin1_2_coin2_df = data_loader._three_markets_data_loader__load_csv_data('./tests/test_files/BTCUSDT-Spot.csv', './tests/test_files/ETHUSDT-Spot.csv', './tests/test_files/ETHBTC-Spot.csv')
+
+    def test_deduplication(self):
+        assert len(self.coin1_df) == 309559
+        assert len(self.coin2_df) == 309559
+        assert len(self.coin1_2_coin2_df) == 309559
+
+    def test_sort(self):
+        coin1_results = [self.coin1_df['date'][index - 1] < self.coin1_df['date'][index] for index in range(1, len(self.coin1_df))]
+        coin2_results = [self.coin2_df['date'][index - 1] < self.coin2_df['date'][index] for index in range(1, len(self.coin2_df))]
+        coin1_2_coin2_results = [self.coin1_2_coin2_df['date'][index - 1] < self.coin1_2_coin2_df['date'][index] for index in range(1, len(self.coin1_2_coin2_df))]
+        assert all(coin1_results)
+        assert all(coin2_results)
+        assert all(coin1_2_coin2_results)
